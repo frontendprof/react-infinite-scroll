966d7afb22ccb835996bce6ff733a76904f81704
frontendprof <abdumaliksharipov@gmail.com>
2020-11-02 14:28:49 +0500

    Creating and working on new component useBooksearch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 
-import React from "react";
+import React, { useState } from "react";
+
+import UseBookSearch from "./UseBookSearch";
 
 
 
 
 function App() {
+
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const handleSearch = (e) = {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+
+
+  UseBookSearch();
+
+
+
   return (
     <>
-      <input type="text"></input>
+      <input type="text" onChange={handleSearch}></input>
       <div>Title</div>
       <div>Title</div>
       <div>Title</div>
---
+++ src/UseBookSearch.jsx
@@ -0,0 +1,21 @@
+
+import { useEffect, useState } from 'react';
+import axios from "axios";
+
+
+
+
+export default function UseBookSearch(query, pageNumber) {
+
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url: "http://openlibrary.org/search.json",
+            params: { q: query, page: pageNumber }
+        }).then(res => {
+            console.log(res.data);
+        })
+    }, [query, pageNumber])
+
+    return null
+}
